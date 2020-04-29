de72b3d7f64d5d9c4a3a6674c19bb029cc790399
manu <manu.villar.vitoria@gmail.com>
2020-04-29 22:57:12 +0200

    show error if any text field is empty

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Variables
 const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
 //console.log(presupuestoUsuario);
@@ -27,6 +28,24 @@ class Interfaz{
         presupuestoSpan.innerHTML = `${cantidad}`;
         restanteSpan.innerHTML = `${cantidad}`;
     }
+
+    imprimirMensaje(mensaje,tipo) {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+        divMensaje.appendChild(document.createTextNode(mensaje));
+        //Insertar en el DOM
+        document.querySelector('.primario').insertBefore(divMensaje,formulario);
+        //Quitar el alert después de 3 seg.
+        setTimeout(() => {
+            document.querySelector('.primario .alert').remove();
+            formulario.reset();
+        },3000);
+    }
 }
 
 
@@ -43,5 +62,26 @@ document.addEventListener('DOMContentLoaded', function(){
        //Instanciar la clase de interfaz
        const ui = new Interfaz();
        ui.insertarPresupuesto(cantidadPresupuesto.presupuesto)
+    }
+});
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    //console.log('Enviado');
+
+    //Leer del formulario de gastos
+    const nombreGasto = document.querySelector('#gasto').value;
+    const cantidadGasto = document.querySelector('#cantidad').value;
+
+    //Instanciar la interfaz
+    const ui = new Interfaz();
+
+    //Comprobar que los campos no estén vacíos
+    if(nombreGasto === '' || cantidadGasto === ''){
+        //console.log('hubo un error');
+        //2 parámetros mensaje y tipo
+        ui.imprimirMensaje('Hubo un error','error')
+    } else {
+        console.log('el gasto se agrego')
     }
 });
